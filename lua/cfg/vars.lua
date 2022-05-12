c9753e3fddc5cc325b90501e4b35e1ca83c4b21e
Maximilian Kienitz <max@kienitz.dev>
2022-05-12 03:26:52 +0200

    Migrate to stow

---
+++ lua/cfg/vars.lua
@@ -0,0 +1,16 @@
+vim.opt.compatible = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.encoding = "utf-8"
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.cmd('colorscheme gruvbox')
