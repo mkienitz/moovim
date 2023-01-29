f0f9e41e3fd5f5f306fb814321a87626e9f9f963
Maximilian Kienitz <max@kienitz.dev>
2023-01-29 23:11:49 +0100

    Adjust diagnostics keybinds

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -74,8 +74,8 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
 	vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-	vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, opts)
-	vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist, opts)
+	vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
 	vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
 	vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
 
