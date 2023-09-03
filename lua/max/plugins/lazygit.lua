a71d76e28b150dd374497c3523640f5fc1553e7e
Maximilian Kienitz <max@kienitz.dev>
2023-09-03 07:19:20 +0200

    feat(nvim): add lazygit selector

--- lua/max/plugins/lazygit.lua
+++ lua/max/plugins/lazygit.lua
@@ -1,12 +1,26 @@
 return {
-	"kdheepak/lazygit.nvim",
-	keys = {
-		{
-			"<leader>gg",
-			function()
-				require("lazygit").lazygit()
-			end,
-			desc = "Open lazygit",
-		},
-	},
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+    },
+    lazy = false,
+    keys = {
+        {
+            "<leader>gs",
+            function()
+                require("telescope").extensions.lazygit.lazygit()
+            end,
+            desc = "Select Lazygit repository",
+        },
+        {
+            "<leader>gg",
+            function()
+                require("lazygit").lazygit()
+            end,
+            desc = "Open lazygit",
+        },
+    },
+    config = function()
+        require("telescope").load_extension("lazygit")
+    end,
 }
