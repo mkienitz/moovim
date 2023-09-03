3a0ce7199435e012e03eead6c3a4ad68d20629a7
Maximilian Kienitz <max@kienitz.dev>
2023-09-03 10:54:39 +0200

    feat(nvim): enable TS-textobjects, bufferline, nvimtree

--- lua/max/core/keymap.lua
+++ lua/max/core/keymap.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<leader>w", "<C-w>")
-vim.keymap.set("n", "<leader>nt", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
