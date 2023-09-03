4ac7414ac4cb4121489353b4e8c920ebc1f32550
Maximilian Kienitz <max@kienitz.dev>
2023-09-03 10:55:05 +0200

    chore(nvim): cleanup lualine

--- lua/max/plugins/lualine.lua
+++ lua/max/plugins/lualine.lua
@@ -1,10 +1,8 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "kyazdani42/nvim-web-devicons", opt = true },
+	dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
 	opts = {
 		options = {
-			component_separators = "|",
-			icons_enabled = false,
 			theme = "gruvbox",
 		},
 	},
