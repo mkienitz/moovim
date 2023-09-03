1173b214504dd653831b3efea1cdc716a488a14a
Maximilian Kienitz <max@kienitz.dev>
2023-09-04 01:43:33 +0200

    feat(nvim): re-enable undotree

---
+++ lua/max/plugins/undotree.lua
@@ -0,0 +1,9 @@
+return {
+  "mbbill/undotree",
+  config = function()
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>u"] = { "<cmd>UndotreeToggle<CR>", "Toggle UndoTree" },
+    })
+  end
+}
