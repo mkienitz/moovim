45fc7c0ee3f25f51fc6dd5da7c8bc584cbf836d3
Maximilian Kienitz <max@kienitz.dev>
2023-09-03 06:28:09 +0200

    feat(nvim): create and adjust Harpoon keybinds

--- lua/max/plugins/harpoon.lua
+++ lua/max/plugins/harpoon.lua
@@ -1,52 +1,48 @@
-local ui = function()
-	return require("harpoon.ui")
+local gen_keys = function()
+    local res = {
+        {
+            "<leader>ha",
+            function()
+                require("harpoon.mark").add_file()
+            end,
+            desc = "Harpoon add mark",
+        },
+        {
+            "<leader>hm",
+            function()
+                require("harpoon.ui").toggle_quick_menu()
+            end,
+            desc = "Harpoon show menu",
+        },
+        {
+            "<leader>hp",
+            function()
+                require("harpoon.ui").nav_previous()
+            end,
+            desc = "Harpoon previous",
+        },
+        {
+            "<leader>hn",
+            function()
+                require("harpoon.ui").nav_next()
+            end,
+            desc = "Harpoon next",
+        },
+    }
+    for i = 0, 9 do
+        table.insert(res, {
+            "<leader>h" .. i,
+            function()
+                require("harpoon.ui").nav_file(i)
+            end,
+            desc = "Jump to file " .. i,
+        })
+    end
+    return res
 end
 
 return {
-	"theprimeagen/harpoon",
-	keys = {
-		{
-			"<leader>a",
-			function()
-				require("harpoon.mark").add_file()
-			end,
-			desc = "Mark file",
-		},
-		{
-			"<C-e>",
-			function()
-				ui().toggle_quick_menu()
-			end,
-			desc = "Toggle menu",
-		},
-		{
-			"<C-h>",
-			function()
-				ui().nav_file(1)
-			end,
-			desc = "Jump to file 1",
-		},
-		{
-			"<C-j>",
-			function()
-				ui().nav_file(2)
-			end,
-			desc = "Jump to file 2",
-		},
-		{
-			"<C-k>",
-			function()
-				ui().nav_file(3)
-			end,
-			desc = "Jump to file 3",
-		},
-		{
-			"<C-l>",
-			function()
-				ui().nav_file(4)
-			end,
-			desc = "Jump to file 4",
-		},
-	},
-	config = true,
+    "theprimeagen/harpoon",
+    keys = gen_keys(),
+    config = true,
 }
