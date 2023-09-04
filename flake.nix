74fbe189a38ea6597f22617eaa712295a33d3a34
Maximilian Kienitz <max@kienitz.dev>
2023-09-05 01:51:26 +0200

    feat: add luacheck for static analysis

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [stylua];
+          packages = with pkgs; [stylua luaPackages.luacheck];
         };
       }
     );
