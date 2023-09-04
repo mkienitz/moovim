5ccf61dc34c688762c5cde02ebe64de356969526
Maximilian Kienitz <max@kienitz.dev>
2023-09-04 20:43:46 +0200

    fix(options): disable expandtab

--- lua/max/core/options.lua
+++ lua/max/core/options.lua
@@ -23,9 +23,9 @@ vim.opt.undofile = true
 vim.opt.autoindent = true
 vim.opt.wrap = false
 vim.opt.smartindent = true
-vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.cmd("syntax off")
 
 vim.o.timeout = true
