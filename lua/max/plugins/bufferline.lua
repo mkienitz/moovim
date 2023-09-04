0dbc4680a9fb7392e50f6cae5ad4c98b8e4b791e
Maximilian Kienitz <max@kienitz.dev>
2023-09-04 05:11:21 +0200

    feat(bufferline): further customize style and behavior

--- lua/max/plugins/bufferline.lua
+++ lua/max/plugins/bufferline.lua
@@ -1,10 +1,25 @@
 return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+  config = function()
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        style_preset = bufferline.style_preset.minimal,
+        diagnostics = "nvim_lsp",
+        close_command = false,
+        right_mouse_command = false,
+        left_mouse_command = false,
+        middle_mouse_command = false,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "neo-tree",
+            separator = true,
+          },
+        },
+        show_buffer_close_icons = false,
+      },
+    })
+  end,
 }
