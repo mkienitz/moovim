07a64a6dc1378be141e25bfc80b33262e16798c8
Maximilian Kienitz <max@kienitz.dev>
2023-09-04 21:02:55 +0200

    feat(nix): add direnv nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    # use release 23.05 branch of the GitHub repository as input, this is the most common input format
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [stylua];
+        };
+      }
+    );
+}
