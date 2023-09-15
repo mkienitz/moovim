43a3404208c36533f7cbb8f636038ca9cca27b83
Maximilian Kienitz <max@kienitz.dev>
2023-09-15 06:57:41 +0200

    feat(keymap): add keymaps to paste into new line

--- lua/max/core/keymap.lua
+++ lua/max/core/keymap.lua
@@ -6,3 +6,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("n", "<leader>p", "<cmd>pu<CR>")
+vim.keymap.set("n", "<leader>P", "<cmd>pu!<CR>")
