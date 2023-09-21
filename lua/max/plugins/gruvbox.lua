4715717d3d6bc5b4030ec1bea6361486c6f95570
Maximilian Kienitz <max@kienitz.dev>
2023-09-21 06:45:44 +0200

    fix(gruvbox): remove opts

--- lua/max/plugins/gruvbox.lua
+++ lua/max/plugins/gruvbox.lua
@@ -2,9 +2,7 @@ return {
 	"ellisonleao/gruvbox.nvim",
 	lazy = false,
 	priority = 1000,
-	opts = {
-		italic = {},
-	},
+	opts = {},
 	config = function(_, opts)
 		require("gruvbox").setup(opts)
 		vim.o.background = "dark"
