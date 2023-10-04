e002e954ebb4edd03cbfda3a9dbdf50c47bd33fe
Maximilian Kienitz <max@kienitz.dev>
2023-10-05 01:59:34 +0200

    chore: disable bufferline for now

--- lua/max/plugins/bufferline.lua
+++ lua/max/plugins/bufferline.lua
@@ -1,6 +1,7 @@
 return {
 	"akinsho/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	enabled = false,
 	config = function()
 		local bufferline = require("bufferline")
 		bufferline.setup({
