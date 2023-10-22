641ae6c50e783a4ebca2486fdc4e7b7c5ae15210
Maximilian Kienitz <max@kienitz.dev>
2023-10-22 15:51:02 +0200

    refactor(flake): remove shellHook, set formatter

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    # use release 23.05 branch of the GitHub repository as input, this is the most common input format
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -14,11 +13,16 @@
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
+        formatter = pkgs.alejandra;
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [alejandra deadnix nil lua-language-server lua stylua luaPackages.luacheck];
-          shellHook = ''
-            git config --local core.hooksPath .hooks
-          '';
+          packages = with pkgs; [
+            deadnix
+            lua
+            lua-language-server
+            luaPackages.luacheck
+            nil
+            stylua
+          ];
         };
       }
     );
